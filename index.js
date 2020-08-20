a795e8b44fd6ddb6a24d6b9dc8de0cfaa87f552e
ryan1522 <ryan1522@naver.com>
2020-08-20 11:30:50 +0900

    bot upload

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : "";
-const welcomeChannelName = "어서-오세요";
-const byeChannelName = "어서-오세요";
+const token = 'process.env. token';
+const welcomeChannelName = "안녕하세요";
+const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
 const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
-  client.user.setPresence({ game: { name: '우산봇 문의는 디엠으로' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
@@ -18,7 +17,7 @@ client.on("guildMemberAdd", (member) => {
 
   welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
 
-  member.addRole(guild.roles.find(role => role.name == "빛나는 조각"));
+  member.addRole(guild.roles.find(role => role.name == "게스트"));
 });
 
 client.on("guildMemberRemove", (member) => {
@@ -41,7 +40,7 @@ client.on('message', (message) => {
     let embed = new Discord.RichEmbed()
       .setTitle('타이틀')
       .setURL('http://www.naver.com')
-      .setAuthor('우산', img, 'http://www.naver.com')
+      .setAuthor('나긋해', img, 'http://www.naver.com')
       .setThumbnail(img)
       .addBlankField()
       .addField('Inline field title', 'Some value here')
@@ -51,23 +50,22 @@ client.on('message', (message) => {
       .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
       .addBlankField()
       .setTimestamp()
-      .setFooter('우산이 만듬', img)
+      .setFooter('나긋해가 만듬', img)
 
     message.channel.send(embed)
-  } else if(message.content == 'help') {
+  } else if(message.content == 'embed2') {
     let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
     let commandList = [
       {name: 'ping', desc: '현재 핑 상태'},
       {name: 'embed', desc: 'embed 예제1'},
       {name: 'embed2', desc: 'embed 예제2 (help)'},
       {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
-      {name: '!청소', desc: '텍스트 지움'},
     ];
     let commandStr = '';
     let embed = new Discord.RichEmbed()
-      .setAuthor('Help of 우산 BOT', helpImg)
+      .setAuthor('Help of 콜라곰 BOT', helpImg)
       .setColor('#186de6')
-      .setFooter(`우산 BOT ❤️`)
+      .setFooter(`콜라곰 BOT ❤️`)
       .setTimestamp()
     
     commandList.forEach(x => {
@@ -93,43 +91,6 @@ client.on('message', (message) => {
       return message.reply('채널에서 실행해주세요.');
     }
   }
-
-  if(message.content.startsWith('!청소')) {
-    if(checkPermission(message)) return
-
-    var clearLine = message.content.slice('!청소 '.length);
-    var isNum = !isNaN(clearLine)
-
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
-      return;
-    } else if(!isNum) { // c @나긋해 3
-      if(message.content.split('<@').length == 2) {
-        if(isNaN(message.content.split(' ')[2])) return;
-
-        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
-        var count = parseInt(message.content.split(' ')[2])+1;
-        const _limit = 10;
-        let _cnt = 0;
-
-        message.channel.fetchMessages({limit: _limit}).then(collected => {
-          collected.every(msg => {
-            if(msg.author.id == user) {
-              msg.delete();
-              ++_cnt;
-            }
-            return !(_cnt == count);
-          });
-        });
-      }
-    } else {
-      message.channel.bulkDelete(parseInt(clearLine)+1)
-        .then(() => {
-          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
-        })
-        .catch(console.error)
-    }
-  }
 });
 
 function checkPermission(message) {
@@ -150,14 +111,6 @@ function changeCommandStringLength(str, limitLen = 8) {
   }
 
   return tmp;
-}
-
-async function AutoMsgDelete(message, str, delay = 3000) {
-  let msg = await message.channel.send(str);
-
-  setTimeout(() => {
-    msg.delete();
-  }, delay);
 }
 
 
